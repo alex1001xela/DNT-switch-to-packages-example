5bcacb9ff0e448ad71bca6a1e1778b23c6ff7e2d
Alexandros Konstantakos <alexander.k@zoho.com>
2018-10-08 21:39:37 +0200

    First commit

---
+++ ClassLibrary2/ClassLibrary2/Class2.cs
@@ -0,0 +1,9 @@
+﻿using ClassLibrary1;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        private Class1 class1Instance;
+    }
+}
---
+++ MainApp/MainApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary1;
+using ClassLibrary2;
+using System;
+
+namespace MainApp
+{
+    class Program
+    {
+        private Class1 class1Instance;
+        private Class2 class2Instance;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
